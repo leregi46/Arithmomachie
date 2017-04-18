fc6e66d8d541d3f2d0d899867abba54b78adca70
leregi46 <regix15@gmail.com>
2017-04-17 19:40:26 -0700

    initialisation du projet

---
+++ arithmomachie.c
@@ -0,0 +1,11 @@
+#include 
+
+int main(int argc, char const *argv[])
+{
+
+
+    if (!init_bibliotheque()) // nap start tout sann bezwen
+        return 7;
+	dessiner_fenetre("ARITHMOMACHIE",1240,750);
+	return 0;
+}
